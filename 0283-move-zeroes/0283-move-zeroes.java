ea3687e59ea563f766dd405130ab56931d7ab0b9
vtu28668 <vtu28668@veltech.edu.in>
2026-09-10 11:51:17 +0530

    Time: 2 ms (91.79%), Space: 48 MB (20.18%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        while (index < nums.length) {
+            nums[index] = 0;
+            index++;
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
