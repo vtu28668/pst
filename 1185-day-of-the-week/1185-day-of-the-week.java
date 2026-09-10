ad54eb2d25f8caacfe75ace96d9358d70d77fdeb
vtu28668 <vtu28668@veltech.edu.in>
2026-09-10 11:36:57 +0530

    Time: 12 ms (6.83%), Space: 43.2 MB (5.87%) - LeetHub

---
+++ 1185-day-of-the-week/1185-day-of-the-week.java
@@ -0,0 +1,17 @@
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+class Solution {
+    public String dayOfTheWeek(int day, int month, int year) {
+        LocalDate date = LocalDate.of(year, month, day);
+
+        return date.getDayOfWeek()
+                   .getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
