62625579b2775e2961cd5d26434682e704fb940e
vtu28668 <vtu28668@veltech.edu.in>
2026-09-10 11:42:15 +0530

    Force committed solution - Time: O(n log n), Space: O(n)

---
+++ 13817-java-comparator-Hackerrank/13817-java-comparator.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+class Player {
+    String name;
+    int score;
+
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class Checker implements Comparator<Player> {
+
+    @Override
+    public int compare(Player a, Player b) {
+
+        // Higher score comes first
+        if (a.score != b.score) {
+            return Integer.compare(b.score, a.score);
+        }
+
+        // Same score -> alphabetical name
+        return a.name.compareTo(b.name);
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Player[] players = new Player[n];
+
+        for (int i = 0; i < n; i++) {
+            String name = sc.next();
+            int score = sc.nextInt();
+
+            players[i] = new Player(name, score);
+        }
+
+        // Sort using Checker
+        Arrays.sort(players, new Checker());
+
+        // Print sorted players
+        for (Player player : players) {
+            System.out.println(player.name + " " + player.score);
+        }
+
+        sc.close();
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
